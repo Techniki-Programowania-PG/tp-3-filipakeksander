108a547f5a15ee7d198695d10e10ca42eabf7ac0
ZAXgames420 <olekkargol@gmail.com>
2025-05-19 16:25:49 +0200

    dodano sygnał piłokształtny plus hotfixy

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ std::vector<std::vector<double> > generuj_sin(double czestotliwosc,int ilosc_pun
 }
 
 std::vector<std::vector<double> > generuj_cos(double czestotliwosc,int ilosc_punktow, double x_min, double x_maks, double przesun_faz){
-    return generuj_sin(czestotliwosc, ilosc_punktow, x_min, x_maks, przesun_faz + 1,57);
+    return generuj_sin(czestotliwosc, ilosc_punktow, x_min, x_maks, przesun_faz + 1.57);
 }
 
 std::vector<std::vector<double> > generuj_prostokatny(double czestotliwosc, int wypelnienie, int ilosc_punktow, double x_min, double x_maks){
@@ -42,17 +42,37 @@ std::vector<std::vector<double> > generuj_prostokatny(double czestotliwosc, int 
     return sygnal;
 }
 
+std::vector<std::vector<double> > generuj_piloksztaltny(double czestotliwosc, int ilosc_punktow, double x_min, double x_maks){
+    std::vector<std::vector<double> >sygnal;
+    sygnal.resize(2);
+    double step = (x_maks - x_min) / (ilosc_punktow - 1);
+    double okres = 1.0/czestotliwosc;
+    double ilosc_w_okresie = okres/step;
+    double wartosc = -1;
+    for(int i=0;i<ilosc_punktow;i++){
+        sygnal[0].push_back(x_min + i * step);
+        sygnal[1].push_back(wartosc);
+        wartosc += 2/ilosc_w_okresie;
+        if(wartosc>1){
+            wartosc=-1;
+        }
+    }
+    return sygnal;
+}
+
 void wyswietl(std::vector<double> x,std::vector<double> y){
     using namespace matplot;
     plot(x, y)->color({0.f, 0.7f, 0.9f});
     xlim({x[0],x.back()});
-    ylim({-2, +2});
+    //ylim({-2, +2});
     show();
 }
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, k) {
     k.def("generuj_sin",&generuj_sin);
+    k.def("generuj_cos",&generuj_cos);
     k.def("wyswietl",&wyswietl);
     k.def("generuj_prostokatny", &generuj_prostokatny);
+    k.def("generuj_piloksztaltny", &generuj_piloksztaltny);
 }
--- src/tp3_filipaleksander/__init__.py
+++ src/tp3_filipaleksander/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from ._core import  generuj_sin, wyswietl, generuj_prostokatny, generuj_cos
+from ._core import  generuj_sin, wyswietl, generuj_prostokatny, generuj_cos, generuj_piloksztaltny
 
-__all__ = ["generuj_sin","wyswietl","generuj_prostokatny","generuj_cos"]
+__all__ = ["generuj_sin","wyswietl","generuj_prostokatny","generuj_cos", "generuj_piloksztaltny"]
